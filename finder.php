14ea7fb8d19b9fdf3ac4bb1eaefbca984f94a1e9
Daniele Scasciafratte <mte90net@gmail.com>
2020-08-04 18:55:24 +0200

    testing exclusion

--- finder.php
+++ finder.php
@@ -1,9 +1,8 @@
 <?php
 
 return \StubsGenerator\Finder::create()
-    ->in('source/vendor/wp-cli/wp-cli')
-    ->append(\StubsGenerator\Finder::create()->files()->depth('< 4')->in(['source/vendor/wp-cli/wp-cli/php']))
-    ->notPath('*/ComposerIO.php')
-    ->notPath('*/PackageManagerEventSubscriber.php')
+    ->in('source/vendor/wp-cli/wp-cli/php')
+    ->notPath('ComposerIO.php')
+    ->notPath('PackageManagerEventSubscriber.php')
     ->sortByName()
 ;
