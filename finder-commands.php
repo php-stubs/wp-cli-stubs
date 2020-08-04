8041d2d58dfa27e0ea5becfe9843ed68c6597090
Daniele Scasciafratte <mte90net@gmail.com>
2020-08-04 18:29:54 +0200

    improve fidner detection

--- finder-commands.php
+++ finder-commands.php
@@ -2,7 +2,7 @@
 
 return \StubsGenerator\Finder::create()
     ->in('source/vendor/wp-cli')
-    ->append(\StubsGenerator\Finder::create()->files()->depth('< 2')->in(['source/vendor/wp-cli/*-command/src']))
+    ->append(\StubsGenerator\Finder::create()->files()->depth('< 2')->in(['source/vendor/wp-cli/*/src']))
     ->notPath('tests')
     ->sortByName()
 ;
