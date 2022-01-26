705d0cc16c22d7ee3e319e846d2fa049e0fdbf08
Viktor Szépe <viktor@szepe.net>
2022-01-26 12:51:51 +0000

    Exclude wp-settings-cli.php for PHP 8

--- finder.php
+++ finder.php
@@ -4,6 +4,7 @@ return \StubsGenerator\Finder::create()
     ->in('source/vendor/wp-cli/wp-cli/php')
     // Compatiblility code
     ->notPath('compat.php')
+    ->notPath('wp-settings-cli.php')
     // FeedbackMethodTrait is used from ->notPath('WP_CLI/Compat')
     // These two need composer/composer
     ->notPath('WP_CLI/ComposerIO.php')
